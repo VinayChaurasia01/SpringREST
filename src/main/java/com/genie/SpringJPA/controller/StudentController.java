6adf7fcb201ddc7d1acb73ce01fc36d0118c96b0
VinayChaurasia01 <vinaychaurasia097@gmail.com>
2025-03-22 16:01:04 +0530

    Vinay Chaurasia Added All CRUD opration by using Rest

--- src/main/java/com/genie/SpringJPA/controller/StudentController.java
+++ src/main/java/com/genie/SpringJPA/controller/StudentController.java
@@ -5,6 +5,8 @@ import com.genie.SpringJPA.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("api/std")
 public class StudentController {
@@ -17,6 +19,31 @@ public class StudentController {
         return service.saveStudent(student);
     }
 
-    /*@GetMapping("/get")
-    public Student create*/
+    @GetMapping("/getAll")
+    public List<Student> create(){
+       return service.getAllStudent();
+    }
+
+    @GetMapping("/{id}")
+    public Student findByID(@PathVariable Integer id){
+        return service.getByID(id);
+    }
+
+    @PutMapping("/{id}")
+    public Student updateStudent(@PathVariable Integer id ,@RequestBody Student student){
+        Student updatedStu = service.updateStudent(id,student);
+        return updatedStu;
+    }
+
+    @DeleteMapping("/deleteAll")
+    public void delete(){
+        service.deleteAllStudent();
+    }
+
+
+    @DeleteMapping("{id}")
+    public void deleteById(@PathVariable int id){
+        service.deleteStudentById(id);
+    }
+
 }
--- src/main/java/com/genie/SpringJPA/service/StudentService.java
+++ src/main/java/com/genie/SpringJPA/service/StudentService.java
@@ -5,6 +5,9 @@ import com.genie.SpringJPA.repo.StudentRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class StudentService {
 
@@ -15,4 +18,48 @@ public class StudentService {
     public Student saveStudent(Student student){
         return studentRepo.save(student);
     }
+
+    public List<Student> getAllStudent(){
+         List<Student> studentsList =studentRepo.findAll();
+         return studentsList;
+    }
+
+
+    public Student getByID(Integer id){
+
+        Optional<Student> ID = studentRepo.findById(id);
+        Student student = ID.get();
+        return student;
+    }
+
+    public Student updateStudent(Integer id , Student student){
+
+        Optional<Student> studentById = studentRepo.findById(id);
+
+        Student s = null;
+
+        if(studentById.isPresent()){
+            Student dbStudentObject = studentById.get();
+            dbStudentObject.setAge(student.getAge());
+            dbStudentObject.setEmail(student.getEmail());
+            dbStudentObject.setId(student.getId());
+            dbStudentObject.setName(student.getName());
+
+            s = studentRepo.save(dbStudentObject);
+        }else{
+            System.out.println("Data not present in DataBase");
+        }
+
+        return s;
+    }
+
+    public void deleteAllStudent(){
+        studentRepo.deleteAll();
+    }
+
+    public void deleteStudentById(int id){
+        studentRepo.deleteById(id);
+    }
+
+
 }
